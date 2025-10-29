1c7885c327374386fb9b995b3f5320d74f7082fe
YE_desktop <12202340@inha.edu>
2025-10-29 09:38:16 +0900

    251029

--- src/main/java/com/kt/repository/UserRepository.java
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -15,7 +15,7 @@ public class UserRepository {
 	private final JdbcTemplate jdbcTemplate;
 
 	public void save(User user) {
-		// 서비스에서 dto를 도메인(비지니스모델)으로 바꾼다음 전달
+		// 서비스에서 dto를 도메인(비지니스모델)으로 바꾼다음  전달
 		var sql = "INSERT INTO MEMBER (loginId, password, name, birthday) VALUES (?, ?, ?, ?)";
 
 		jdbcTemplate.update(sql, user.getLoginId(), user.getPassword(), user.getName(), user.getBirthday());
